d13ae8d741ab464722e2d8c7318bd01103e1a2f6
root <1187503260@qq.com>
2023-10-12 15:18:41 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.l6 = new System.Windows.Forms.Label();
+            this.l1 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.l4 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.l5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // l6
+            // 
+            this.l6.AutoSize = true;
+            this.l6.BackColor = System.Drawing.Color.Transparent;
+            this.l6.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.l6.Location = new System.Drawing.Point(12, 66);
+            this.l6.Name = "l6";
+            this.l6.Size = new System.Drawing.Size(42, 19);
+            this.l6.TabIndex = 6;
+            this.l6.Text = "- -";
+            this.l6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.l6.Click += new System.EventHandler(this.l6_Click);
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.BackColor = System.Drawing.Color.Transparent;
+            this.l1.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.l1.Location = new System.Drawing.Point(12, 14);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(42, 19);
+            this.l1.TabIndex = 8;
+            this.l1.Text = "- -";
+            this.l1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l3
+            // 
+            this.l3.AutoSize = true;
+            this.l3.BackColor = System.Drawing.Color.Transparent;
+            this.l3.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.l3.Location = new System.Drawing.Point(12, 35);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(42, 19);
+            this.l3.TabIndex = 8;
+            this.l3.Text = "- -";
+            this.l3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l4
+            // 
+            this.l4.AutoSize = true;
+            this.l4.BackColor = System.Drawing.Color.Transparent;
+            this.l4.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.l4.Location = new System.Drawing.Point(12, 45);
+            this.l4.Name = "l4";
+            this.l4.Size = new System.Drawing.Size(42, 19);
+            this.l4.TabIndex = 9;
+            this.l4.Text = "- -";
+            this.l4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.BackColor = System.Drawing.Color.Transparent;
+            this.l2.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.l2.Location = new System.Drawing.Point(12, 24);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(42, 19);
+            this.l2.TabIndex = 10;
+            this.l2.Text = "- -";
+            this.l2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l5
+            // 
+            this.l5.AutoSize = true;
+            this.l5.BackColor = System.Drawing.Color.Transparent;
+            this.l5.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.l5.Location = new System.Drawing.Point(12, 55);
+            this.l5.Name = "l5";
+            this.l5.Size = new System.Drawing.Size(42, 19);
+            this.l5.TabIndex = 11;
+            this.l5.Text = "- -";
+            this.l5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.l5);
+            this.groupBox1.Controls.Add(this.l6);
+            this.groupBox1.Controls.Add(this.l2);
+            this.groupBox1.Controls.Add(this.l1);
+            this.groupBox1.Controls.Add(this.l4);
+            this.groupBox1.Controls.Add(this.l3);
+            this.groupBox1.Location = new System.Drawing.Point(49, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(67, 100);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "地为坤";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(166, 199);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "取挂";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label l6;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label l4;
+        private System.Windows.Forms.Label l2;
+        private System.Windows.Forms.Label l5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string[] d = { "000000", "000001", "000010", "000011", "000100", "000101", "000110", "000111", "001000", "001001", "001010", "001011", "001100", "001101", "001110", "001111", "010000", "010001", "010010", "010011", "010100", "010101", "010110", "010111", "011000", "011001", "011010", "011011", "011100", "011101", "011110", "011111", "100000", "100001", "100010", "100011", "100100", "100101", "100110", "100111", "101000", "101001", "101010", "101011", "101100", "101101", "101110", "101111", "110000", "110001", "110010", "110011", "110100", "110101","110110", "110111", "111000", "111001", "111010", "111011", "111100", "111101", "111110", "111111",  };
+        private string[] f = { "地为坤", "地雷复", "地水师", "地泽临", "地山谦", "地火明夷", "地风升", "地天泰", "雷地豫", "震为雷", "雷水解", "雷泽归妹", "雷山小过", "雷火丰", "雷风恒", "雷天大壮", "水地比", "水雷屯", "坎为水", "水泽节", "水山蹇", "水火既济", "水风井", "水天需", "泽地萃", "泽雷随", "泽水困", "兑为泽", "泽山咸", "泽火革", "泽风大过", "泽天央", "山地剥", "山雷颐", "山水蒙", "山泽损", "艮为山", "山火贲", "山风蛊", "山天大畜", "火地晋", "火雷噬嗑", "火水未济", "火泽瞬", "火山旅", "离为火", "火风鼎", "火天大有", "风地观", "风雷益", "风水涣", "风泽中孚", "风山渐", "风火家人", "翼为风", "风天小畜", "天地否", "天雷无妄", "天水讼", "天泽履", "天山遁", "天火同人", "天风始", "乾为天", };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            string[] b = new string[6];
+            string c = "";
+            for (int i = 0; i < 6; i++)
+            {
+                string a =  rd.Next(0,2).ToString();
+                if (a == "1")
+                {
+                    b[i] = "---";
+                    System.Threading.Thread.Sleep(10);
+                }
+                if (a == "0")
+                {
+                    b[i] = "- -";
+                    System.Threading.Thread.Sleep(10);
+                }
+
+                c += a;
+            }
+            l1.Text = b[0];
+            l2.Text = b[1];
+            l3.Text = b[2];
+            l4.Text = b[3];
+            l5.Text = b[4];
+            l6.Text = b[5];
+
+            int ia = 0;
+            int ib = 0;
+            for (ia = 0; ia < 64; ia++)
+            {
+                if (c == d[ia])
+                {
+                    ib = ia;
+                    break;
+                }
+            }
+            label1.Text = f[ib];
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void l6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
